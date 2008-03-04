cea3013711a935630a0106b7378ba2c076b56ecd
Leif Roschier <leif.roschier@avion.fi>
2008-03-04 21:45:00 +0000

    still to do work in nomo_grid_box.py

--- pynomo/pynomo/nomo_grid_box.py
+++ pynomo/pynomo/nomo_grid_box.py
@@ -27,7 +27,7 @@ import time
 
 class Nomo_Grid_Box(object):
     """
-    class to draw "grid boxes" according to (East)-German nomographies
+    class to calculate "grid boxes" according to (East)-German nomographic tradition
     """
     def __init__(self,params={}):
         """
@@ -47,47 +47,71 @@ class Nomo_Grid_Box(object):
                                }
         self.params=params_default_values
         self.params.update(params)
+        # initial guesses
         self.u_func=self.params['u_func']
         self.v_func=self.params['v_func']
-        #testing
-        #self.line,self.sections=self._build_x_line(p=1.0)
-        self._scale_funcs_vertical_()
-        self._build_v_lines_()
+        self._build_v_lines_(self.v_func)
         self._calc_bound_box_ini_()
-        self._build_u_lines_()
-
-#        v_func_temp=self.v_func
-#        self.v_func=lambda x,v:v_func_temp((x-self.x_left_ini)/\
-#                                           (self.x_right_ini-self.x_left_ini)*\
-#                                           self.params['width'],v)
-#        self._scale_funcs_vertical_()
-#        self._build_v_lines_()
-#        self._calc_bound_box_ini_()
-#        self._build_u_lines_()
-
+        self._build_u_lines_(self.u_func)
+        #debug by looking pdf
         self._draw_debug_ini_()
+        # build scaled versions
+        self._scale_()
+        self._draw_debug_ini_('after.pdf')
 
-    def _scale_ini_to_width_height_(self):
+    def give_u_data(self):
         """
-        scales initial coordinates to width and to height
+        gives x(u),y(u)-functions and scale values
         """
-        # bounding box initial have to still ok
         pass
 
-    def _scale_funcs_vertical_(self):
+    def give_v_data(self):
         """
-        scales functions such that maximum value gives 1.0 and minimum 0.0
+        gives x(v),y(v)-functions and scale values and corresponding titles
+        function is linear and values are set manually to correct places
         """
-        max_fu=self.u_func(self.params['u_values'][0])
-        min_fu=max_fu
-        for u in self.params['u_values']:
-            fu=self.u_func(u)
-            if fu>max_fu:
-                max_fu=fu
-            if fu<min_fu:
-                min_fu=fu
-        self.u_func = lambda u:(self.params['u_func'](u)-min_fu)/(max_fu-min_fu)*self.params['height']
-        self.v_func = lambda x,v:(self.params['v_func'](x,v)-min_fu)/(max_fu-min_fu)*self.params['height']
+        pass
+
+    def give_w_data(self):
+        """
+        gives x(w),y(w)-functions and w_min and w_max values.
+        """
+        pass
+
+    def give_wd_data(self):
+        """
+        gives x(wd),y(wd)-functions and wd_min and wd_max values.
+        """
+        pass
+
+    def give_contours(self):
+        """
+        gives contours to be drawn
+        """
+        pass
+
+
+    def _scale_(self):
+        """
+        scales everything to width and height
+        """
+        # scales lines
+        y_factor=self.params['height']/self.BB_height_ini
+        x_factor=self.params['width']/self.BB_width_ini
+        for idx1,u_line in enumerate(self.u_lines):
+            for idx2,(x,y) in enumerate(u_line):
+                x_new=x*x_factor
+                y_new=y*y_factor
+                self.u_lines[idx1][idx2]=(x_new,y_new)
+        for idx1,v_line in enumerate(self.v_lines):
+            for idx2,(x,y) in enumerate(v_line):
+                x_new=x*x_factor
+                y_new=y*y_factor
+                self.v_lines[idx1][idx2]=(x_new,y_new)
+        # scale functions
+        self.u_func=lambda u:self.params['u_func'](u)*y_factor
+        self.v_func=lambda x,v:self.params['v_func'](x/x_factor,v)*y_factor
+
 
     def _build_u_scale_(self):
         """
@@ -101,13 +125,14 @@ class Nomo_Grid_Box(object):
         """
         pass
 
-    def _build_u_lines_(self):
+    def _build_u_lines_(self,u_func):
         """
         lines starting from u scale
         """
         self.u_lines=[]
         self.u_sections=[]
-        g=self.u_func
+        #g=self.u_func
+        g=u_func
         for u in self.params['u_values']:
             line = [(self.x_left_ini, g(u))]
             line.append((self.x_left_ini, g(u)))
@@ -116,27 +141,37 @@ class Nomo_Grid_Box(object):
             self.u_lines.append(line)
             self.u_sections.append(section)
 
-    def _build_v_lines_(self):
+    def _build_v_lines_(self,v_func):
         """
         build lines starting from top scale
         """
         self.v_lines=[]
         self.v_sections=[]
         for p in self.params['v_values']:
-            line,sections = self._build_v_line_(p)
+            line,sections = self._build_v_line_(v_func=v_func,p=p)
             self.v_lines.append(line)
             self.v_sections.append(sections)
 
-    def _build_v_line_(self,p=1.0):
+    def _build_v_line_(self,v_func,p=1.0):
         """
         line starting from x scale
         code copied originally form nomo_axis_func.py: _calculate_points_
+        v_func is the functio(x,p)
         p is the parametric value of the top scale
         """
-        #func2=self.params['v_func']
-        func2=self.v_func
-        func_top=lambda x:(func2(x,p)-self.params['height'])**2 # minimum at height
-        func_bottom=lambda x:(func2(x,p))**2 # minimum at 0.0
+        # find top and bottom lines
+        max_fu=self.u_func(self.params['u_values'][0])
+        min_fu=max_fu
+        for u in self.params['u_values']:
+            fu=self.u_func(u)
+            if fu>max_fu:
+                max_fu=fu
+            if fu<min_fu:
+                min_fu=fu
+        # functions to find x-values in paper
+        func2=v_func
+        func_top=lambda x:(func2(x,p)-max_fu)**2 # minimum at height
+        func_bottom=lambda x:(func2(x,p)-min_fu)**2 # minimum at 0.0
         f=lambda x:x
         g=lambda x:func2(x,p)
         # find point of scale to meet point 1.0
@@ -151,7 +186,7 @@ class Nomo_Grid_Box(object):
         stop=max(x_top,x_bottom)
         du=fabs(stop-start)*1e-12
         # approximate line length is found
-        line_length_straigth=1.0
+        line_length_straigth=max_fu-min_fu
         sections=200.0 # number of sections
         section_length=line_length_straigth/sections
         line = [(f(start), g(start))]
@@ -225,6 +260,8 @@ class Nomo_Grid_Box(object):
         self.x_right_ini=x_right
         self.y_top_ini=y_top
         self.y_bottom_ini=y_bottom
+        self.BB_width_ini=x_right-x_left
+        self.BB_height_ini=y_top-y_bottom
         return x_left,x_right,y_bottom,y_top
 
 
@@ -262,8 +299,20 @@ class Nomo_Grid_Box(object):
         cc.writePDFfile(filename)
 
 if __name__=='__main__':
+
+    def f1(x,u):
+        return log(x/(x-u/100.0))/log(1+u/100.0)
+
+    params={'width':10.0,
+           'height':10.0,
+           'u_func':lambda u:u,
+           'v_func':f1,
+           'u_values':[10.0,20.0,30.0,40.0,50.0,60.0],
+           'v_values':[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0],
+           'manual_axis_data':None # manual labels
+           }
     tic = time.time()
-    test=Nomo_Grid_Box()
+    test=Nomo_Grid_Box(params=params)
     toc = time.time()
     print toc-tic,' has elapsed'
 
